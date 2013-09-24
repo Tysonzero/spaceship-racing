85cea172cf372ce0381b1b28f7ebdc81bbff9352
Tysonzero <danielspencersmith@gmail.com>
2013-09-24 12:46:05 -0600

    Add position and speed variables to spaceship

--- scripts/vars/spaceship.js
+++ scripts/vars/spaceship.js
@@ -1,7 +1,19 @@
 var spaceship = {
+  //position of the spaceship in pixels
+  pos:{
+    x:0,
+    y:0,
+  },
+  
+  //speed of the spaceship in pixels per frame
+  speed:{
+    x:0,
+    y:0,
+  },
+  
   //variable manipulation
   update:function() {
-  
+    
   },
   
   //drawing to screen
