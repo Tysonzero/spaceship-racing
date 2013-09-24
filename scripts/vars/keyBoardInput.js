84abefaddda54ed252acdc28846f0987945b0d48
Tysonzero <danielspencersmith@gmail.com>
2013-09-24 09:27:10 -0600

    Create keyBoardInput.js

---
+++ scripts/vars/keyBoardInput.js
@@ -0,0 +1,12 @@
+var keyBoardInput = {
+  initialize:function() {
+    this._bind_keys();
+  },
+  
+  _bind_keys:function() {
+    //called whenever a key is pressed
+    window.onkeydown = function(e) {
+    
+    }
+  },
+}
