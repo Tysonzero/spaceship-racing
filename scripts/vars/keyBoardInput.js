1cb9069d5d24cec527931e1f2c6ea2f49b2dfa78
Tysonzero <danielspencersmith@gmail.com>
2013-09-24 09:37:59 -0600

    Add WASD controls

--- scripts/vars/keyBoardInput.js
+++ scripts/vars/keyBoardInput.js
@@ -6,7 +6,19 @@ var keyBoardInput = {
   _bind_keys:function() {
     //called whenever a key is pressed
     window.onkeydown = function(e) {
-    
+      switch(e.keyCode) {
+        //called when A key is pressed
+        case 65:
+          break;
+        //called when D key is pressed
+        case 68:
+          break;
+        //called when S key is pressed
+        case 83:
+          break;
+        //called when W key is pressed
+        case 87:
+          break;
     }
   },
 }
