8304e2faf9857bc41ffbf2ed81a53b4dee5d9f93
Tysonzero <danielspencersmith@gmail.com>
2013-09-25 20:48:06 -0600

    Make speed change when accelerate is true

--- scripts/vars/spaceship.js
+++ scripts/vars/spaceship.js
@@ -5,21 +5,24 @@ var spaceship = {
   //direction of the spaceship in radians
   direction:0,
   
-  //position of the spaceship in pixels
-  pos:{
-    x:0,
-    y:0,
-  },
-  
   //speed of the spaceship in pixels per frame
   speed:{
     x:0,
     y:0,
   },
   
+  //position of the spaceship in pixels
+  pos:{
+    x:0,
+    y:0,
+  },
+  
   //variable manipulation
   update:function() {
-    
+    if(this.accelerate) {
+      this.speed.x += Math.cos(this.direction);
+      this.speed.y += Math.sin(this.direction);
+    }
   },
   
   //drawing to screen
